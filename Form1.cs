604a1e77f1ea31241bd2c353ecbdfd509013215c
Ana Sánchez <nuski.sanjim@gmail.com>
2016-05-06 13:52:16 +0200

    Agregación de un MessageBox y un comentario.

--- Form1.cs
+++ Form1.cs
@@ -15,6 +15,9 @@ namespace Git_Ex1
         {
 			// Modificación desde otro repositorio
             InitializeComponent();
+
+            //Cambio realizado por Ana Sánchez
+            MessageBox.Show("¡Realizado!", "cambio", MessageBoxButtons.OK);
         }
 
         private void button1_Click(object sender, EventArgs e)
@@ -28,6 +31,11 @@ namespace Git_Ex1
 			// Segunda modificación 2016 en clase
 
             // Modificación realizada el dia 4/5/2016 - Alumno 1º DAW - "Felipe Ruz Perez"
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
